aec80883eadb557722062480189cf136e5682fd6
covid19warriors <covid19warriors@hos03mac05.local>
2020-04-15 10:47:05 +0200

    define reagents

--- functions.py
+++ functions.py
@@ -2,57 +2,101 @@ opentrons_simulate /Users/covid19warriors/Documents/covid19clinic/Station\ B/Sta
 
 
 class Reagent:
-    def __init__(self,name,flow_rate_aspirate,flow_rate_dispense,rinse,reagent_reservoir,num_wells,h_cono,v_fondo,tip_recycling='A1'):
-        self.name=name
-        self.flow_rate_aspirate=flow_rate_aspirate
-        self.flow_rate_dispense=flow_rate_dispense
-        self.rinse=bool(rinse)
-        self.reagent_reservoir=reagent_reservoir
-        self.num_wells=num_wells
-        self.col=0
-        self.vol_well=0
-        self.h_cono=h_cono
-        self.v_cono=v_fondo
-        self.tip_recycling=tip_recycling
+    def __init__(self, name, flow_rate_aspirate, flow_rate_dispense, rinse,
+    reagent_reservoir, num_wells, h_cono, v_fondo, tip_recycling = 'none'):
+        self.name = name
+        self.flow_rate_aspirate = flow_rate_aspirate
+        self.flow_rate_dispense = flow_rate_dispense
+        self.rinse = bool(rinse)
+        self.reagent_reservoir = reagent_reservoir
+        self.num_wells = num_wells
+        self.col = 0
+        self.vol_well = 0
+        self.h_cono = h_cono
+        self.v_cono = v_fondo
+        self.tip_recycling = tip_recycling
     def vol_well_original(self):
         return self.reagent_reservoir/self.num_wells
 
-Ethanol=Reagent('Ethanol',0.5,1,True,12000,4,,tip_recycling='A1') #num_Wells max is 4
-Beads=Reagent('Magnetic beads',0.5,1,True,12000,4,,tip_recycling='A2') #num_Wells max is 4
-Isopropanol=Reagent('Isopropanol',0.5,1,True,5000,2,,tip_recycling='A3') #num_Wells max is 2
-Water=Reagent('Water',1,1,False,6000,1,,) #num_Wells max is 1
-Elution=Reagent('Elution',0.25,1,False,800,num_cols,,) #num_cols comes from available columns
+#Reagents and their characteristics
+Ethanol = Reagent(name = 'Ethanol',
+                flow_rate_aspirate = 0.5,
+                flow_rate_dispense = 1,
+                rinse = True,
+                reagent_reservoir = 12000,
+                num_wells = 4, #num_Wells max is 4
+                h_cono = 2,
+                v_fondo = 50,
+                tip_recycling = 'A1')
+
+Beads = Reagent(name = 'Magnetic beads',
+                flow_rate_aspirate = 0.5,
+                flow_rate_dispense = 1,
+                rinse = True,
+                reagent_reservoir = 12000,
+                num_wells = 4,
+                h_cono = ,
+                v_fondo = ,
+                tip_recycling = 'A2')
+
+Isopropanol = Reagent(name = 'Isopropanol',
+                flow_rate_aspirate = 0.5,
+                flow_rate_dispense = 1,
+                rinse = True,
+                reagent_reservoir = 5000,
+                num_wells = 2, #num_Wells max is 2
+                h_cono = ,
+                v_fondo = ,
+                tip_recycling = 'A3')
+
+Water = Reagent(name = 'Water',
+                flow_rate_aspirate = 1,
+                flow_rate_dispense = 1,
+                rinse = False,
+                reagent_reservoir = 6000,
+                num_wells = 1, #num_Wells max is 1
+                h_cono = ,
+                v_fondo = )
+
+Elution = Reagent(name = 'Elution',
+                flow_rate_aspirate = 0.25,
+                flow_rate_dispense = 1,
+                rinse = False,
+                reagent_reservoir = 800,
+                num_wells = num_cols, #num_cols comes from available columns
+                h_cono = ,
+                v_fondo = )
 
 [custom_mix(p,r,s,vol) for _ in range(5)]
 
-def custom_mix(pipet,reagent,source,destination,vol):
-    pipet.flow_rate.aspirate=reagent.flow_rate_aspirate
-    pipet.aspirate(vol,source)
-    pipet.flow_rate.dispense=reagent.flow_rate_dispense
-    pipet.dispense(vol,destination)
-    pipet.dispense(30, dest.top(z=-5))
+def custom_mix(pipet, reagent, location, vol, rounds, blow_out):
+    for _ in range(rounds):
+        pipet.aspirate(vol, location, rate = reagent.flow_rate_aspirate)
+        pipet.dispense(vol, location, rate = reagent.flow_rate_dispense)
+    if blow_out == True:
+        pipet.blow_out(location.top(z = -2)) # Blow out
 
 beads = reagent_res.rows()[0][:Beads.num_wells] # 1 row, 4 columns (first ones)
-isoprop = reagent_res.rows()[0][4:(4+Isopropanol.num_wells)] # 1 row, 2 columns (from 5 to 6)
+isoprop = reagent_res.rows()[0][4:(4 + Isopropanol.num_wells)] # 1 row, 2 columns (from 5 to 6)
 etoh = reagent_res.rows()[0][6:Ethanol.num_wells] # 1 row, 2 columns (from 7 to 10)
 water = reagent_res.rows()[0][-1] # 1 row, 1 column (last one) full of water
 work_destinations = deepwell_plate.rows()[0][:Elution.num_wells]
 final_destinations = elution_plate.rows()[0][:Elution.num_wells]
 
-    def calc_height(reagent,cross_section_area,aspirate_volume,):
-        if reagent.vol_well<aspirate_volume:
-            reagent.vol_well=reagent.vol_well_original-aspirate_volume
-            height=(reagent.vol_well-reagent.v_cono)/cross_section_area-reagent.h_cono
-            reagent.col=reagent.col+1 # column selector position; intialize to required number
-            if height<0:
-                height=0.1
-        else:
-            height=(reagent.vol_well-reagent.v_cono)/cross_section_area-reagent.h_cono
-            reagent.col=0+reagent.col
-            reagent.vol_well=reagent.vol_well-aspirate_volume
-            if height<0:
-                height=0.1
-        return height
+def calc_height(reagent, cross_section_area, aspirate_volume,):
+    if reagent.vol_well < aspirate_volume:
+        reagent.vol_well = reagent.vol_well_original - aspirate_volume
+        height = (reagent.vol_well-reagent.v_cono)/cross_section_area-reagent.h_cono
+        reagent.col = reagent.col+1 # column selector position; intialize to required number
+        if height < 0:
+            height = 0.1
+    else:
+        height=(reagent.vol_well - reagent.v_cono)/cross_section_area-reagent.h_cono
+        reagent.col = 0 + reagent.col
+        reagent.vol_well = reagent.vol_well-aspirate_volume
+        if height < 0:
+            height = 0.1
+    return height
 
 tip_recycle = [ctx.load_labware('opentrons_96_tiprack_300ul', '5', '200µl filter tiprack')]
 
