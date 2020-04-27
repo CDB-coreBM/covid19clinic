79c3b070b39758c9c33a58bf126f888d901cccd9
covid19warriors <jlvillanueva@clinic.cat>
2020-04-27 16:20:47 +0200

    log name

--- Kingfisher_protocols/KF_pathogen/Station_KA_SampleSetup_pathogen_v1.py
+++ Kingfisher_protocols/KF_pathogen/Station_KA_SampleSetup_pathogen_v1.py
@@ -47,7 +47,7 @@ def run(ctx: protocol_api.ProtocolContext):
     folder_path = '/var/lib/jupyter/notebooks'
     if not os.path.isdir(folder_path):
         os.mkdir(folder_path)
-    file_path = folder_path + '/KA_time_log.txt'
+    file_path = folder_path + '/KA_SampleSetup_pathogen_time_log.txt'
 
     # Define Reagents as objects with their properties
     class Reagent:
