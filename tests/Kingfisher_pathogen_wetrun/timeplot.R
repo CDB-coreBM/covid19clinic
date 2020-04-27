58e951be61d337c3c44237662cb574c3ca61f4da
covid19warriors <jlvillanueva@clinic.cat>
2020-04-27 17:54:53 +0200

    last plot no mix in station A

--- tests/Kingfisher_pathogen_wetrun/timeplot.R
+++ tests/Kingfisher_pathogen_wetrun/timeplot.R
@@ -2,7 +2,7 @@ library(lubridate)
 library(tidyverse)
 #install.packages("kableExtra")
 
-data_A <- read_delim("/Users/covid19warriors/Documents/code/covid19clinic/tests/Kingfisher_pathogen_wetrun/KA_sample_setup_pathogen_time_log.txt", "\t", escape_double = FALSE, col_types = cols(execution_time = col_character()), trim_ws = TRUE)
+data_A <- read_delim("/Users/covid19warriors/Documents/code/covid19clinic/tests/Kingfisher_pathogen_wetrun/KA_SampleSetup_pathogen_time_log.txt", "\t", escape_double = FALSE, col_types = cols(execution_time = col_character()), trim_ws = TRUE)
 data_A$station <- 'A: sample setup'
 data_B1 <- read_delim("/Users/covid19warriors/Documents/code/covid19clinic/tests/Kingfisher_pathogen_wetrun/KB_sample_prep_pathogen_log.txt", "\t", escape_double = FALSE, col_types = cols(execution_time = col_character()), trim_ws = TRUE)
 data_B1$station <- 'B1: sample prep'
@@ -26,9 +26,10 @@ ggplot(data,aes(x=fct_reorder(fct_reorder(new_desc, STEP),station), y=Time)) +
   ylab('Time in minutes')+
   scale_fill_brewer(palette = "Set1")
 
-ggsave('kingfisher_times_test_27042020_.png',plot=last_plot())
+ggsave('/Users/covid19warriors/Documents/code/covid19clinic/tests/Kingfisher_pathogen_wetrun/kingfisher_times_test_27042020_nomix.png', height=6, width=9, plot=last_plot())
 
 table1 <- data %>%
   group_by(station) %>%
   summarize(steps=n(),total_time=sum(as.double(Time))/60)
 
+write.table(table1, file="/Users/covid19warriors/Documents/code/covid19clinic/tests/Kingfisher_pathogen_wetrun/table1_times.txt",row.names = FALSE, sep='\t', quote = FALSE)
