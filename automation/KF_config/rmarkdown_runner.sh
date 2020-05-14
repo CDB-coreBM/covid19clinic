c22ca2d9b4985c6909e5691da1f0991be6875867
egenomics <jlvillanueva@clinic.cat>
2020-05-14 18:18:44 +0200

    up

---
+++ automation/KF_config/rmarkdown_runner.sh
@@ -0,0 +1,11 @@
+
+
+$run
+$csv_file
+
+folder='/run/user/1003/gvfs/smb-share:server=opn.cdb.nas.csc.es,share=opentrons/RUNS/'
+run='2020_05_14_OT4_KF/results/2020_05_14_OT4_KF_Presence Absence Result.csv'
+Rscript -e 'library(rmarkdown); rmarkdown::render("/home/jl/Documentos/code/covid19clinic/automation/test_rmarkdown.Rmd", "html_document", output_file = "OT4_resultados.html", output_dir="/run/user/1003/gvfs/smb-share:server=opn.cdb.nas.csc.es,share=opentrons/RUNS/2020_05_14_OT4_KF/results/")'
+
+
+/run/user/1003/gvfs/smb-share:server=opn.cdb.nas.csc.es,share=opentrons/RUNS/2020_05_14_OT4_KF/results/2020_05_14_OT4_KF_Presence Absence Result.csv
