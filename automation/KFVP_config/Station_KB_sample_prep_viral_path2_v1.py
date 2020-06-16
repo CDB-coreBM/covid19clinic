705372c83a78e2bc4262fe0b3cdcdd5fea20230d
egenomics <jlvillanueva@clinic.cat>
2020-06-16 10:21:11 +0200

    update tech scripts

--- automation/KFVP_config/Station_KB_sample_prep_viral_path2_v1.py
+++ automation/KFVP_config/Station_KB_sample_prep_viral_path2_v1.py
@@ -311,17 +311,6 @@ def run(ctx: protocol_api.ProtocolContext):
                     STEPS[STEP]['description'] + ' took ' + str(time_taken))
         STEPS[STEP]['Time:'] = str(time_taken)
 
-    # Export the time log to a tsv file
-    if not ctx.is_simulating():
-        with open(file_path, 'w') as f:
-            f.write('STEP\texecution\tdescription\twait_time\texecution_time\n')
-            for key in STEPS.keys():
-                row = str(key)
-                for key2 in STEPS[key].keys():
-                    row += '\t' + format(STEPS[key][key2])
-                f.write(row + '\n')
-        f.close()
-
     ############################################################################
     # STEP 2: TRANSFER BEADS
     ############################################################################
@@ -355,7 +344,7 @@ def run(ctx: protocol_api.ProtocolContext):
                                       dest=work_destinations_cols[i], vol=transfer_vol,
                                       air_gap_vol=air_gap_vol, x_offset=x_offset,
                                       pickup_height=pickup_height, disp_height = -2,
-                                      rinse=rinse, blow_out = False, touch_tip=False)
+                                      rinse=rinse, blow_out = False, touch_tip = False)
 
         m300.drop_tip(home_after=False)
         tip_track['counts'][m300] += 8
