c14c6f7aa3cae668771b27985bc91a24f5f09425
egenomics <jlvillanueva@clinic.cat>
2020-09-02 14:15:29 +0200

    change field new folder

--- automation/KFVP_config/rmarkdown_runner.sh
+++ automation/KFVP_config/rmarkdown_runner.sh
@@ -4,7 +4,8 @@ run=$1
 #folder='/run/user/1003/gvfs/smb-share:server=opn.cdb.nas.csc.es,share=opentrons/RUNS/'
 folder="/run/user/1003/gvfs/smb-share:server=cscfs2,share=usr2/USERS/OPENTRONS/RUNS/"
 
-rscript=`find ${folder}${run} -name '*.Rmd' -print0 | xargs -r0 echo | cut -d '/' -f10`
+#rscript=`find ${folder}${run} -name '*.Rmd' -print0 | xargs -r0 echo | cut -d '/' -f10`
+rscript=`find ${folder}${run} -name '*.Rmd' -print0 | xargs -r0 echo | cut -d '/' -f12`
 Rscript -e 'library(rmarkdown);
 rmarkdown::render("'${folder}${run}'/scripts/'${rscript}'",
 "html_document", output_file = "'${run}'_resultados.html",
