47c3a44171019c3744447d90550bf21cdb779223
egenomics <jlvillanueva@clinic.cat>
2020-09-02 14:07:56 +0200

    fix bug path2

--- automation/KFVP_config/rmarkdown_runner.sh
+++ automation/KFVP_config/rmarkdown_runner.sh
@@ -2,7 +2,7 @@
 # set path to watch
 run=$1
 #folder='/run/user/1003/gvfs/smb-share:server=opn.cdb.nas.csc.es,share=opentrons/RUNS/'
-folder="/run/user/1003/gvfs/smb-share:server=cscfs2,share=usr2/USERS/OPENTRONS/RUNS"
+folder="/run/user/1003/gvfs/smb-share:server=cscfs2,share=usr2/USERS/OPENTRONS/RUNS/"
 
 rscript=`find ${folder}${run} -name '*.Rmd' -print0 | xargs -r0 echo | cut -d '/' -f10`
 Rscript -e 'library(rmarkdown);
