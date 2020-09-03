91de7a27a33655695e9722ba0024118185ba77d8
egenomics <jlvillanueva@clinic.cat>
2020-09-03 11:24:20 +0200

    revert to original folder

--- automation/KFVP_config/rmarkdown_runner.sh
+++ automation/KFVP_config/rmarkdown_runner.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 # set path to watch
 run=$1
-#folder='/run/user/1003/gvfs/smb-share:server=opn.cdb.nas.csc.es,share=opentrons/RUNS/'
-folder="/run/user/1003/gvfs/smb-share:server=cscfs2,share=usr2/USERS/OPENTRONS/RUNS/"
+folder='/run/user/1003/gvfs/smb-share:server=opn.cdb.nas.csc.es,share=opentrons/RUNS/'
+#folder="/run/user/1003/gvfs/smb-share:server=cscfs2,share=usr2/USERS/OPENTRONS/RUNS/"
 
-#rscript=`find ${folder}${run} -name '*.Rmd' -print0 | xargs -r0 echo | cut -d '/' -f10`
-rscript=`find ${folder}${run} -name '*.Rmd' -print0 | xargs -r0 echo | cut -d '/' -f12`
+rscript=`find ${folder}${run} -name '*.Rmd' -print0 | xargs -r0 echo | cut -d '/' -f10`
+#rscript=`find ${folder}${run} -name '*.Rmd' -print0 | xargs -r0 echo | cut -d '/' -f12` #backup folder
 Rscript -e 'library(rmarkdown);
 rmarkdown::render("'${folder}${run}'/scripts/'${rscript}'",
 "html_document", output_file = "'${run}'_resultados.html",
